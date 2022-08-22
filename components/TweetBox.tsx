4e88b58c2be60ad8451ce3e1ca5c5ef945426cba
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2022-08-22 12:45:51 +0200

    Update TweetBox.tsx

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -9,7 +9,7 @@ import {
 
 function TweetBox() {
   return (
-    <div>
+    <div className="flex space-x-2 p-5">
       <img
         className="mt-4 h-14 w-14 rounded-full object-cover"
         src="https://links.papareact.com/gll"
@@ -17,7 +17,7 @@ function TweetBox() {
 
       <div className="flex flex-1 items-center pl-2">
         <form className="flex flex-1 flex-col">
-          <input type="text" placeholder="What's happening?" />
+          <input type="text" placeholder="What's happening?" className="h-24"/>
           <div className="flex items-center">
             <div className="flex space-x-2 text-twitter flex-1">
               <PhotographIcon className="h-5 w-5" />
