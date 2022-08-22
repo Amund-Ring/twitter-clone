6c55825b760c7d10d6006c2dc083f1b3218aa3c4
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2022-08-22 14:47:21 +0200

    Sanity setup

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   CalendarIcon,
   EmojiHappyIcon,
@@ -8,6 +8,8 @@ import {
 } from "@heroicons/react/outline";
 
 function TweetBox() {
+  const [input, setInput] = useState<string>('')
+  
   return (
     <div className="flex space-x-2 p-5">
       <img
@@ -17,16 +19,24 @@ function TweetBox() {
 
       <div className="flex flex-1 items-center pl-2">
         <form className="flex flex-1 flex-col">
-          <input type="text" placeholder="What's happening?" className="h-24"/>
+          <input
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            type="text"
+            placeholder="What's happening?"
+            className="h-24 text-xl outline-none placeholder:text-xl"
+          />
           <div className="flex items-center">
-            <div className="flex space-x-2 text-twitter flex-1">
-              <PhotographIcon className="h-5 w-5" />
-              <SearchCircleIcon className="h-5 w-5" />
-              <EmojiHappyIcon className="h-5 w-5" />
-              <CalendarIcon className="h-5 w-5" />
-              <LocationMarkerIcon className="h-5 w-5" />
+            <div className="flex flex-1 space-x-2 text-twitter">
+              <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <SearchCircleIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <EmojiHappyIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <CalendarIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <LocationMarkerIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
             </div>
-            <button className="bg-twitter px-5 py-2 font-bold text-white rounded-full">Tweet</button>
+            <button disabled={!input} className="disabled:opacity-40 rounded-full bg-twitter px-5 py-2 font-bold text-white">
+              Tweet
+            </button>
           </div>
         </form>
       </div>
