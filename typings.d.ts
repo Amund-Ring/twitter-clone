20c5efb99716583caa5f3c0e8df63964e44f3250
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2022-09-21 14:52:44 +0200

    fetchComments

--- typings.d.ts
+++ typings.d.ts
@@ -4,7 +4,7 @@ export interface Tweet extends TweetBody {
   _updatedAt: string;
   _rev: string;
   _type: "tweet";
-  blockTweet: boolean
+  blockTweet: boolean;
 }
 
 export type TweetBody = {
@@ -13,3 +13,22 @@ export type TweetBody = {
   profileImg: string;
   image?: string;
 };
+
+export type CommentBody = {
+  comment: string;
+  tweetId: string;
+  username: string;
+  profileImg: string;
+};
+
+export interface Comment extends CommentBody {
+  _id: string;
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  _type: "comment";
+  tweet: {
+    _ref: string
+    _type: 'reference'
+  }
+}
